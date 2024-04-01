da87c12cdbc1dff8cb3b4e91876a421d7c4d0e3e
web-kamrul <webkamrul0001@gmail.com>
2024-04-01 14:22:59 +0600

    First commit

---
+++ src/components/VarifyForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VarifyForm = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default VarifyForm;
---
+++ src/pages/CartListPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartListPage = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default CartListPage;
